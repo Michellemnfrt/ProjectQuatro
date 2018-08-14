723811e87fcd1674183c9708baaa0f92e9ac1f44
Michelle Monfort <Michellemnfrt@gmail.com>
2018-08-14 15:20:56 -0400

    remove .env

--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michellemonfort/wdi/projects/4/car_app/car_project/.env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
